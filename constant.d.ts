5b71dae879e92a92b9bd5fde6d9b6a1be2304ee6
Gandum2077 <gandum2077@hotmail.com>
2024-02-13 22:30:04 +0800

    bugfix: fix the bug of the constant.d.ts

--- constant.d.ts
+++ constant.d.ts
@@ -53,9 +53,9 @@ declare const $zero: {
 };
 
 declare const $layout: {
-    fill: (make: MASConstraintMaker, view: AllUIView) => void;
-    fillSafeArea: (make: MASConstraintMaker, view: AllUIView) => void;
-    center: (make: MASConstraintMaker, view: AllUIView) => void;
+    fill: (make: MASConstraintMaker, view: UIView) => void;
+    fillSafeArea: (make: MASConstraintMaker, view: UIView) => void;
+    center: (make: MASConstraintMaker, view: UIView) => void;
 };
 
 declare const $lineCap: {
