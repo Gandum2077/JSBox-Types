e1e6ac127aaf95190fa29ac2a3fd5a9a191f0d7b
Gandum2077 <gandum2077@hotmail.com>
2024-07-11 11:19:15 +0800

    bugfix

--- picker.d.ts
+++ picker.d.ts
@@ -7,15 +7,32 @@ interface Picker {
             min?: Date;
             max?: Date;
             mode?: number;
-            interval: number;
+            interval?: number;
         }
     }): Promise<Date>;
+    date(args?: {
+        props: {
+            date?: Date;
+            min?: Date;
+            max?: Date;
+            mode?: number;
+            interval?: number;
+        },
+        handler: (date: Date) => void;
+    }): void;
     data(args: {
         props: {
             items: any[];
         }
     }): Promise<string[]>;
+    data(args: {
+        props: {
+            items: any[];
+        },
+        handler: (index: number) => void;
+    }): void;
     color(args?: { color: UIColor }): Promise<UIColor>; // 参数为默认颜色
+    color(args?: { color: UIColor }, handler: (color: UIColor) => void): void; // 参数为默认颜色
 }
 
 declare const $picker: Picker;
